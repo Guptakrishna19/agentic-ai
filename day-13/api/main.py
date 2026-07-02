bf9536a5e715d6a72fa82cdaaa33750619aee4a0
dhruvL@21 <dhruvlukhi07@gmail.com>
2026-07-02 16:25:17 +0530

    connected the full flow of fetching data from backend using frontend

--- day-13/api/main.py
+++ day-13/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from database import get_connection
 from models import Employee
 
@@ -6,6 +7,14 @@ app = FastAPI(
     title="Employee CRUD API",
     description="FastAPI + SQLite CRUD Example",
     version="1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/employees")
